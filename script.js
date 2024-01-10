ea97ed2e0eab2716f63e8ccbb04b5ac96ba5375b
MD IRTEZA RAHMAN <mdirtezar@gmail.com>
2024-01-10 23:24:44 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+function page3Animation(){
+    var elemCon = document.querySelector("#elem-container")
+    var fixed = document.querySelector("#fixed-image")
+    elemCon.addEventListener("mouseenter", function () {
+        fixed.style.display = "block"
+    })
+    elemCon.addEventListener("mouseleave", function () {
+        fixed.style.display = "none"
+    })
+
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
+            var image = e.getAttribute("data-image")
+            fixed.style.backgroundImage = `url(${image})`
+        })
+    })
+}
+page3Animation()
